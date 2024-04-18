d28423599e7d36fdb5aa7226d3280007aeb0d6c3
ParasY1724 <parasyerunkar174@gmail.com>
2024-04-18 23:55:15 +0530

    #

--- rc_app/apps.py
+++ rc_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ApiConfig(AppConfig):
+class RcAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
+    name = 'rc_app'
