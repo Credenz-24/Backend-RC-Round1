40db6b1e6830c9ab379bcaa102c3e79d1a569594
prasad <hackerspider09@gmail.com>
2024-04-19 02:14:24 +0530

    csrf

--- rc_backend/settings.py
+++ rc_backend/settings.py
@@ -136,11 +136,15 @@ STATIC_ROOT = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOWED_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    'https://dev--rc24.netlify.app', 
-    'https://api.rc.credenz.in', 
-    'https://rc.credenz.in', 
+CORS_ORIGIN_WHITELIST = [
+    'https://rc.credenz.in',
+    'https://api.rc.credenz.in',
+    'http://localhost:5173',
+]
+CSRF_TRUSTED_ORIGINS = [
+    'https://rc.credenz.in',
+    'https://api.rc.credenz.in',
+    'http://localhost:5173',
 ]
 
 
